ea5dada1938047d33463c6ed351e4cd5d82028c5
Spenser Filler <spenserfiller@MacBook-Pro.lan>
2022-11-05 22:56:02 -0500

    remove unused articles

--- themes/advanced/src/config/defaultConfig.ts
+++ themes/advanced/src/config/defaultConfig.ts
@@ -3,16 +3,16 @@ import { SiteConfig } from "./types";
 const config: SiteConfig = {
   // Website configuration
   website: {
-    title: "Gatsby Advanced Starter", // Homepage title
+    title: "SimpleSpore", // Homepage title
     titleShort: "SimpleSpore", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
-    name: "Gatsby Advanced Starter", // Website name used for homescreen (PWA) and SEO
-    description: "A GatsbyJS starter equipped with advanced features.", // Website description used for RSS feeds/meta description tag
+    name: "SimpleSpore", // Website name used for homescreen (PWA) and SEO
+    description: "An insporational place", // Website description used for RSS feeds/meta description tag
     language: "en", // Sets the global HTML lang attribute
     logoUrl: "/logos/logo-1024.png", // Logo used for SEO
-    url: "https://example.com", // Domain of your website without the pathPrefix
+    url: "https://simplespore.com", // Domain of your website without the pathPrefix
     rss: "/rss.xml", // Path to the RSS file
-    rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feed
-    copyright: "© Copyright 2021", // Copyright string for the footer of the website and RSS feed.
+    rssTitle: "SimpleSpore RSS Feed", // Title of the RSS feed
+    copyright: "© Copyright 2022", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#D83850", // Used for setting manifest and progress theme colors.
     backgroundColor: "#F7F7F7", // Used for setting manifest background color.
   },
